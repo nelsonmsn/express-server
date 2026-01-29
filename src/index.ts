eee6645444b5e6a61d5e5876e364802970022e8a
nelsonmsn <isonade.git@gmail.com>
2026-01-29 18:58:21 +0530

    added a post route

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
     res.send("Hello, world. this is a simple express server")
 })
 
+app.post("/users", (req, res) => {
+    const users = [{ name: "John Doe", age: 21 },
+    { name: "Paul Smith", age: 89 }
+    ]
+
+    res.json(users);
+})
+
 app.listen(PORT, () => {
     console.log(`server is listening on ${PORT}`)
 })
